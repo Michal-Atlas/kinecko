6c9ad1f97820ffde13432214aa996fd9d22efd00
Michal Atlas <me+git@michal-atlas.cz>
2025-04-11 02:54:57 +0200

    Swap to CL

--- flake.nix
+++ flake.nix
@@ -3,57 +3,33 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
-    haskell-flake.url = "github:srid/haskell-flake";
   };
   outputs =
     {
-      nixpkgs,
       flake-parts,
-      haskell-flake,
       systems,
       ...
     }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
-      imports = [ haskell-flake.flakeModule ];
       perSystem =
         {
-          self',
           pkgs,
-          config,
-          lib,
           ...
         }:
         {
-          haskellProjects.default = {
-            projectRoot =
-              let
-                fs = lib.fileset;
-              in
-              fs.toSource {
-                root = ./.;
-                fileset = fs.unions [
-                  ./app
-                  ./kinecko.cabal
-                  ./LICENSE
-                ];
-              };
-            packages.themoviedb.source = "1.2.2";
-            settings.themoviedb.jailbreak = true;
-            autoWire = [
-              "packages"
-              "checks"
-              "apps"
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [
+              (pkgs.sbcl.withPackages (
+                lpkgs: with lpkgs; [
+                  # keep-sorted start
+                  dexador
+                  jzon
+                  # keep-sorted end
+                ]
+              ))
             ];
           };
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ config.haskellProjects.default.outputs.devShell ];
-          };
-          packages.default = pkgs.runCommand "kinecko" { } ''
-            . ${pkgs.makeWrapper}/nix-support/setup-hook
-            makeWrapper ${self'.packages.kinecko}/bin/kinecko $out/bin/kinecko \
-                          --suffix PATH : ${lib.makeBinPath [ pkgs.gmic ]}
-          '';
         };
     };
 }
---
+++ kinecko.lisp
@@ -0,0 +1,68 @@
+(require :asdf)
+(require :dexador)
+(require :com.inuoe.jzon)
+
+;; (setf dex:*verbose* t)
+(defvar *api-path* "https://api.themoviedb.org/3")
+(defvar *api-key* (uiop:getenv "TMDB_KEY"))
+(defvar *db-file* #P"movies.txt")
+
+(defstruct movie
+  id title image-url)
+
+(defun movie-path (m)
+  (make-pathname :name (format nil "~a" (movie-id m))
+                 :type "jpg"
+                 :defaults #P"store/"))
+
+(defun movie-poster (m)
+  (let ((path (movie-path m)))
+    (unless (uiop:file-exists-p path)
+      (format t "Downloading poster for ~a... " (movie-title m))
+      (alexandria.2:write-byte-vector-into-file
+       (dex:get (format nil "https://image.tmdb.org/t/p/original~a"
+                        (movie-image-url m)))
+       path)
+      (format t "done~%"))))
+
+(defun id-movie (id)
+  (format t "Downloading movie info for ~a... " id)
+  (let ((table
+          (com.inuoe.jzon:parse
+           (dex:get
+            (format nil
+                    "~a/movie/~a?api_key=~a"
+                    *api-path* id *api-key*)))))
+    (prog1
+        (make-movie :id id
+                    :title (gethash "original_title" table)
+                    :image-url (gethash "poster_path" table))
+      (format t "done~%"))))
+
+(let ((movies
+        (uiop:with-input-file (s *db-file*)
+          (mapcar (lambda (m)
+                    (typecase m
+                      (number (id-movie m))
+                      (t m)
+                      ))
+                  (read s)))))
+  (uiop:with-output-file (s *db-file* :if-exists :supersede)
+    (pprint movies s))
+  (dolist (m movies)
+    (movie-poster m))
+  (format t "Running gmic...~%")
+  (uiop:run-program
+   (append '("gmic")
+           (mapcar (alexandria.2:compose
+                    #'namestring
+                    #'movie-path)
+                   movies)
+           '("rr2d"
+             "2000,1000"
+             "frame"
+             ",3,0,0,0"
+             "pack"
+             "1,-k"
+             "output")
+           '("output.jpg"))))
